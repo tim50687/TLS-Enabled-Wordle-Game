30357e2ba196f0beea82e10133cf7bfa90d8107a
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-01-17 16:46:54 -0500

    verified getaddrinfo

--- client.c
+++ client.c
@@ -3,9 +3,13 @@
 #include <netdb.h>      // for getaddrinfo()
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define NON_TLS_PORT "27993"
 #define TLS_PORT "27994"
+#define SERVER_HOSTNAME "proj1.3700.network"
 
 /*
  * This function is used to display an error message provided in the 'message'
@@ -23,7 +27,7 @@ void error(const char *message)
  */
 void check_input(int argc, char *argv[], char **port_number, char **name_of_server, char **user)
 {
-    if (argc < 3 || argc > 6)
+    if (argc < 3 || argc > 5)
     {
         fprintf(stderr, "Usage: ./client <-p port> <-s> <hostname> <Northeastern-username>\n");
         exit(1);
@@ -58,6 +62,29 @@ void check_input(int argc, char *argv[], char **port_number, char **name_of_serv
     printf("User name: %s\n", *user);
 }
 
+/*
+ * This function prints information from the given addrinfo structure.
+ */
+void print_addrinfo(struct addrinfo *address_info)
+{
+    printf("Family: %d\n", address_info->ai_family);
+    printf("Socket Type: %d\n", address_info->ai_socktype);
+    printf("IP address: ");
+    char ip_str[1024];
+    if (address_info->ai_family == AF_INET)
+    {
+        struct sockaddr_in *ipv4 = (struct sockaddr_in *)address_info->ai_addr;
+        inet_ntop(AF_INET, &(ipv4->sin_addr), ip_str, sizeof(ip_str));
+    }
+    else
+    {
+        struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)address_info->ai_addr;
+        inet_ntop(AF_INET6, &(ipv6->sin6_addr), ip_str, sizeof(ip_str));
+    }
+    printf("%s\n", ip_str);
+    printf("Port: %d\n", ntohs(((struct sockaddr_in *)address_info->ai_addr)->sin_port));
+}
+
 int main(int argc, char *argv[])
 {
     // initialize port number, IP, and user
@@ -67,5 +94,27 @@ int main(int argc, char *argv[])
 
     // First, check user's command line input
     check_input(argc, argv, &port_number, &name_of_server, &user);
+
+    // Get the address information
+    struct addrinfo hints;
+    struct addrinfo *server_info;
+
+    memset(&hints, 0, sizeof(hints)); // set all bytes to 0
+    // Provide preference
+    hints.ai_family = AF_UNSPEC;     // allow both IPv4, IPv6
+    hints.ai_socktype = SOCK_STREAM; // use TCP
+    hints.ai_flags = AI_PASSIVE;     // usually used for server applications, if NULL is provided in 'node' paramter, getaddrinfo will automatically fill in IP
+
+    if ((getaddrinfo(SERVER_HOSTNAME, port_number, &hints, &server_info)) < 0)
+    {
+        error("getaddrinfo error");
+    }
+    else
+    {
+        // print out some useful information to verify getaddrinfo
+        print_addrinfo(server_info);
+    }
+
+    freeaddrinfo(server_info);
     return 0;
 }
