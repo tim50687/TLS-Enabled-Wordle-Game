d5ea8f5bfdf256b3e973d6531bdd16ec8a404be2
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-01-20 15:30:41 -0500

    how brew installed openssl and added TSL function to client.h

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ OBJS = $(SRCS:.c=.o) # Turn every .c file in source to .o
 all: $(TARGET)
 
 client: $(OBJS)
-	$(CC) $(CFLAGS) $(OBJS) -o $(TARGET)
+	$(CC) $(CFLAGS) $(OBJS) -o $(TARGET) 
 
 # Remove the Executable and Object Files
 clean: 
--- client.h
+++ client.h
@@ -17,7 +17,9 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-#include <netdb.h> // For struct addrinfo
+#include <netdb.h>       // For struct addrinfo
+#include <openssl/ssl.h> // For SSL connection
+#include <openssl/err.h>
 
 // Constant definitions
 #define SERVER_HOSTNAME "proj1.3700.network"
@@ -31,5 +33,9 @@ void get_message_from_json(char *message, char *message_json, char *key);
 void setup_connection(const char *hostname, const char *port, int *sockfd);
 void send_hello_message(int sockfd, const char *user, char *buffer);
 void play_game(int sockfd, const char *game_id);
+
+// TSL handshake
+SSL_CTX *create_ssl_context();
+void configure_ssl_context(SSL_CTX *ctx);
 
 #endif // CLIENT_H
